954995532088965ffc382ac879d98b09d7f55ede
omaramr3410 <omaramr3410@gmail.com>
2020-11-07 16:08:21 -0500

    currently, need to start on the control unit design and VHDL description

--- VHDL Code/16bit_ALU.vhdl
+++ VHDL Code/16bit_ALU.vhdl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:185bbd4e6860ebef442fb60911ccbac82b0121fa3b5459085d9c65b0f4639983
-size 2185
+oid sha256:01c05132180bea4b35df0405ab94b8e45047421b0d126a35ccf2aedfaec5f469
+size 2191
--- VHDL Code/datapath_toplevel.vhdl
+++ VHDL Code/datapath_toplevel.vhdl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d00109a21b491569a12a3a613fc31d5642a027e5f457e2afd5b6c0f84e6fcb05
-size 7046
+oid sha256:c84b0dc3b7c959429f2582cf3e7b6ee05ac552b59d18dbe43a327566c3f7d077
+size 7233
