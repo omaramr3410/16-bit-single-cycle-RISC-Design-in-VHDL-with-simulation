326d872bc656f683f6695da68c5a84ddcf18e582
omaramr3410 <omaramr3410@gmail.com>
2020-11-10 23:33:14 -0500

    finished the Control Unit and complete processor VHDL component, need to finish testing datapath with all instructions in Modelsim

--- VHDL Code/ControlUnits/control_unit.vhdl
+++ VHDL Code/ControlUnits/control_unit.vhdl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fc6fd855e83e11934a8e03fb1f069548aaea25c7dc613b9fa1dffbaebde11a2
-size 5630
+oid sha256:baa13ef89d59279fd62a6587c0603cada930f92da57a47c88abf91effb5492a4
+size 6047
